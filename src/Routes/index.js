919ad1d3767c98122f45d3d7755bb46ca2385b21
josphanai0203 <Kaidophan37@gmail.com>
2024-01-04 10:23:15 +0700

    set up form code

---
+++ src/Routes/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Routes } from "react-router-dom";
+import PrivateRouter from "./privateRouter";
+
+const MainRouter = () => {
+  return (
+    <Routes>
+      
+    </Routes>
+  );
+};
+
+export default MainRouter;
