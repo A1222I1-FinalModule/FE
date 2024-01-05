f7f63dd32d1938af7102e2b340775d3fa62fe884
Nhan <nguyenngocbaonhan2003@gmail.com>
2024-01-05 20:59:58 +0700

    setup lib

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
 import MainRouter from "./Routes";
 
 function App() {
-  return <MainRouter/>;
+    return <MainRouter / > ;
 }
 
 export default App;
