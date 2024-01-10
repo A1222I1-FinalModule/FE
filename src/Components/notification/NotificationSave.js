8eaa6c5949d48dd403a81c47a454a8bb28b49754
Phuctam03 <hophuctam7112003@gmail.com>
2024-01-10 18:50:47 +0700

    rebase

--- src/Components/notification/NotificationSave.js
+++ src/Components/notification/NotificationSave.js
@@ -1,5 +1,81 @@
 import React from "react";
+import "./NotificationSave.css";
+import notification from "../../assets/Images/img-03.png";
 
 export default function NotificationSave() {
-  return <div>NotificationSave</div>;
+  return (
+    <>
+      <div class="contact1">
+        <div class="container-contact1">
+          <div class="contact1-pic js-tilt" data-tilt>
+            <img src={notification} alt="Thông báo" />
+          </div>
+
+          <form class="contact1-form">
+            <span class="contact1-form-title"> Đăng thông báo </span>
+
+            <div class="wrap-input1">
+              <textarea class="input1" placeholder="Nội dung"></textarea>
+              <span class="shadow-input1"></span>
+            </div>
+
+            <div class="wrap-input1">
+              <div class="form-check form-check-inline">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="inlineRadioOptions"
+                  id="inlineRadio1"
+                  value="option1"
+                />
+                <label class="form-check-label" for="inlineRadio1">
+                  Tất Cả
+                </label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="inlineRadioOptions"
+                  id="inlineRadio2"
+                  value="option2"
+                />
+                <label class="form-check-label" for="inlineRadio2">
+                  Salers
+                </label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="inlineRadioOptions"
+                  id="inlineRadio3"
+                  value="option1"
+                />
+                <label class="form-check-label" for="inlineRadio3">
+                  Quản Kho
+                </label>
+              </div>
+            </div>
+
+            <div class="container-contact1-form-btn">
+              <div class="contact1-form-btn1">
+                <button class="contact1-form-btn">
+                  <span>
+                    Gửi
+                    <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+                  </span>
+                </button>
+              </div>
+              <div class="contact1-form-btn2">
+                <button class="contact1-form-btn">
+                  <span> Hủy Bỏ </span>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,22 +1,26 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import PrivateRouter from "./privateRouter";
-<<<<<<< HEAD
 import NotificationList from "../Components/notification/NotificationList";
-=======
 import Login from "../Components/Login";
->>>>>>> 561224c38a8a89bbfc34537f8bc4b6089df7d802
+import NotificationSave from "../Components/notification/NotificationSave";
 
 const MainRouter = () => {
   return (
     <Routes>
-<<<<<<< HEAD
       <Route path="/notification" element={<NotificationList />}></Route>
-=======
-      <Route path="/" element={<></>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/private" element={<PrivateRouter><></></PrivateRouter>}/>
->>>>>>> 561224c38a8a89bbfc34537f8bc4b6089df7d802
+      <Route path="/" element={<></>} />
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/private"
+        element={
+          <PrivateRouter>
+            <></>
+          </PrivateRouter>
+        }
+      />
+      <Route path="/notifications" element={<NotificationList />}></Route>
+      <Route path="/notificationsave" element={<NotificationSave />}></Route>
     </Routes>
   );
 };
