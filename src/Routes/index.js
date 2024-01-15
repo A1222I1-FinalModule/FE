4a271b11869aff440348b6fe485271f4e9b7ef5a
Quannisme <121941593+Quannisme@users.noreply.github.com>
2024-01-15 18:46:00 +0700

    update merge

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,31 +1,38 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import PrivateRouter from "./privateRouter";
-import Dashboard from "../Pages/DashBoard";
-import Login from "../Components/Login";
-import Payment from "../Components/Payment"
-import { Toaster} from 'react-hot-toast';
-import { CreateDiscount } from "../Components/Discount/createDiscount";
-import { ListCustomer } from "../Components/Customer/listCustomer";
-import { Discount } from "../Components/Discount/listDiscount";
-import { UpdateDiscount } from "../Components/Discount/updateDiscount";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import PrivateRouter from './privateRouter';
+import Dashboard from '../Pages/DashBoard';
+import Login from '../Components/Login';
+import Payment from '../Components/Payment';
+import { Toaster } from 'react-hot-toast';
+import { CreateDiscount } from '../Components/Discount/createDiscount';
+import { ListCustomer } from '../Components/Customer/listCustomer';
+import { Discount } from '../Components/Discount/listDiscount';
+import { UpdateDiscount } from '../Components/Discount/updateDiscount';
 
 const MainRouter = () => {
-  return (
-    <>
-        <Routes>
-      <Route path="/admin/dashboard" element={<Dashboard></Dashboard>}></Route>      
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/private" element={<PrivateRouter><></></PrivateRouter>}/>
-      <Route path="/createDiscount" element={<CreateDiscount/>}></Route>
-      <Route path="/updateDiscount/:id" element={<UpdateDiscount/>}></Route>
-      <Route path="/listCustomer" element={<ListCustomer/>}></Route>
-      <Route path="/listDiscount" element={<Discount/>}></Route>
-      <Route path="/" element={<></>}/>
-    </Routes>
-    <Toaster position="top-right" reverseOrder={false} />;
-            <Route path="/payment" element={<Payment/>} />
-            </>
+    return (
+        <>
+            <Routes>
+                <Route path="/admin/dashboard" element={<Dashboard></Dashboard>}></Route>
+                <Route path="/login" element={<Login />} />
+                <Route
+                    path="/private"
+                    element={
+                        <PrivateRouter>
+                            <></>
+                        </PrivateRouter>
+                    }
+                />
+                <Route path="/createDiscount" element={<CreateDiscount />}></Route>
+                <Route path="/updateDiscount/:id" element={<UpdateDiscount />}></Route>
+                <Route path="/listCustomer" element={<ListCustomer />}></Route>
+                <Route path="/listDiscount" element={<Discount />}></Route>
+                <Route path="/" element={<></>} />
+            </Routes>
+            <Toaster position="top-right" reverseOrder={false} />;
+            <Route path="/payment" element={<Payment />} />
+        </>
     );
 };
 
