3cb204370de6b836bc59cf109b0afc60c3daf487
thanhbui230202 <thanhbui.230202@gmail.com>
2024-01-26 07:11:10 +0700

    update

--- src/Routes/AdminRoutes.js
+++ src/Routes/AdminRoutes.js
@@ -7,13 +7,14 @@ import { CreateDiscount } from '../Components/Discount/createDiscount';
 import { UpdateDiscount } from '../Components/Discount/updateDiscount';
 import CustomerCreate from '../Components/Customer/CustomerCreate';
 import CustomerUpdate from '../Components/Customer/CustomerUpdate';
-
+import ContentAdmin from '../Components/ContentAdmin';
 const AdminRoutes = () => {
     return (
         <Routes>
             <Route path='/' element={<Dashboard><Info /> </Dashboard>} />
             <Route path='/info' element={<Dashboard><Info /></Dashboard>} />
             <Route path='/discount' element={<Dashboard><Discount /></Dashboard>} />
+            <Route path='/report' element={<Dashboard><ContentAdmin /></Dashboard>} />
             <Route path='/customer' element={<Dashboard><ListCustomer /></Dashboard>} />
             <Route path='/createDiscount' element={<Dashboard><CreateDiscount /></Dashboard>} />
             <Route path='/updateDiscount/:id' element={<Dashboard><UpdateDiscount /></Dashboard>} />
--- src/Routes/SalerRoutes.js
+++ src/Routes/SalerRoutes.js
@@ -6,18 +6,11 @@ import StatisticalTable from '../templates/MonthlyStatisticalTable';
 import Dashboard from '../Pages/DashBoard';
 export default function SalerRoutes() {
     return (
-<<<<<<< HEAD
-        <Routes>       
-            <Route path='/statistical' element={<Saler><StatisticalTable /></Saler>} />
-            <Route path='/info' element={<Saler><Info /></Saler>} />
-            <Route path='/payment' element={<Saler><Payment /></Saler>} />
-=======
         <Routes>
             <Route path='/' element={<Dashboard><Info /></Dashboard>} />
             <Route path='/payment' element={<Dashboard><Payment></Payment></Dashboard>} />
             <Route path='/statistical' element={<Dashboard><StatisticalTable /></Dashboard>} />
             <Route path='/info' element={<Dashboard><Info /></Dashboard>} />
->>>>>>> 42e7896ef8e554450c65f2ea93961999591fb29e
         </Routes>
     );
 };
