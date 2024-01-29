b203d9387ce3e174ff0105fb3636d7fb7ec8f530
Quannisme <121941593+Quannisme@users.noreply.github.com>
2024-01-29 22:23:58 +0700

    the last update

--- src/Components/product/InfoProductCreate.js
+++ src/Components/product/InfoProductCreate.js
@@ -13,6 +13,7 @@ import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { imageDb } from '../../FirebaseProduct/ConfigProduct.js';
 import { v4 } from 'uuid'
 // import imgProduct from '../../Assets/Images/Product/upload-image.webp';
+import imgProduct from '../../Assets/Images/product/upload-image.webp';
 
 
 
